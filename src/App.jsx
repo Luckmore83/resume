7293d233b5baa2bdd4f438ab63f9e079fa91ac6d
Luckmore83 <daaksve@gmail.com>
2025-02-07 00:07:49 +0100

    initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
